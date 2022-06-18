e967a4c6bda611a40a96d9a27d3e7cfa6ebe667a
Ashley's PC <ashlynn4567@gmail.com>
2022-06-18 00:22:47 -0500

    updated index routes with comments and created all category routes in routes/api/category-routes.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,13 @@
+// imports
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// redirect to appropriate file pathway
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
+// exports
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
+// imports
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// redirect routes
 router.use('/api', apiRoutes);
 
+// if incorrect file path is given, send error message
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
+// exports
 module.exports = router;
